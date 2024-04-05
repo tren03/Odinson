95b7f64a8723767817e4164ce0d0920263f259ba
vishnu <vishnusethuraman@gmail.com>
2024-04-05 08:33:49 +0530

    finished the addrow function  to take book as argument and display as column of table

--- library/index.js
+++ library/index.js
@@ -1,14 +1,15 @@
-function Book(author, title, pages, status) { //constructor function to create new objects of same type
+function Book(author, title, pages, status) {
+    //constructor function to create new objects of same type
     (this.author = author),
-    (this.title = title),
-    (this.pages = pages),
-    (this.status = status);
+        (this.title = title),
+        (this.pages = pages),
+        (this.status = status);
 }
 
 let book1 = new Book("mark", "CoolFirstBook", 234, "unread");
 let book2 = new Book("mark", "CoolFirstBook", 234, "unread");
 
-const mylib = [book1, book2]; // array to store the books 
+const mylib = [book1, book2]; // array to store the books
 
 //function to push book into library
 function addBookToLibrary(book) {
@@ -19,42 +20,27 @@ const button = document.getElementById("add");
 
 let book3 = new Book("thesdk", "adkflkj", 235, "adsflk");
 
-button.addEventListener("click",()=>{
+button.addEventListener("click", () => {
     // addBookToLibrary(book3);
     // console.log(mylib.length)
 
-    addRow(); // we will add arguments later
-})
+    addRow(book3); // we will add arguments later
+});
 
 // dynamically adding rows to existing table via js
 
+function addRow(book) {
+    var table = document.getElementById("table");
+    var row = table.insertRow();
 
-function addRow()
-{
-    var table = document.getElementById("table")
-    var row = table.insertRow()
-    var complete_row_data = []
-    for(i=0;i<=3;i++)
-    {
-        var data = document.createElement("td")
-        data.innerHTML = "sample"
-        complete_row_data.push(data)
+    var cellData = [book.author,book.title,book.pages,book.status]
 
-    }
-    for(i=0;i<=3;i++)
-    {
-        row.appendChild(complete_row_data[i])
+    cellData.forEach((data)=>{
+        var cell = document.createElement("td")
+        cell.innerHTML= data;
+        row.appendChild(cell)  
 
-    }
-    
-    
+    })
 
-    
 
 }
-
-
-
-
-
-
