e8555af0525b8caa03b367669822e36869a2da75
vishnu <vishnusethuraman@gmail.com>
2024-04-06 18:57:44 +0530

    fixed the feature of adding books with buttons and tested all edge cases. works fine :)

--- library/index.js
+++ library/index.js
@@ -1,53 +1,74 @@
 function Book(author, title, pages, status) {
-    //constructor function to create new objects of same type
-    (this.author = author),
-        (this.title = title),
-        (this.pages = pages),
-        (this.status = status);
+  //constructor function to create new objects of same type
+  (this.author = author),
+    (this.title = title),
+    (this.pages = pages),
+    (this.status = status);
 }
 
-let book1 = new Book("mark", "CoolFirstBook", 234, "unread");
-let book2 = new Book("mark", "CoolFirstBook", 234, "unread");
 
-popUp = document.getElementById("popupForm")
+popUp = document.getElementById("popupForm");
 
-const mylib = [book1, book2]; // array to store the books
+
+const mylib = []; // array to store the books
+
 
 //function to push book into library
 function addBookToLibrary(book) {
+  if(book.author != "" && book.title != "" && book.pages != "" && book.status!= "" )
+  {
     mylib.push(book);
+  }
+  console.log("please enter the details")
+  
 }
 
 const button = document.getElementById("add");
 
 let book3 = new Book("thesdk", "adkflkj", 235, "adsflk");
 
-button.addEventListener("click", () => {
-    popUp.style.display = "block" //shows the popup
-    document.getElementById("btn").addEventListener("click",()=>{
-        var book = new Book(document.getElementById("author").value,document.getElementById("title").value,document.getElementById("pages").value,document.getElementById("status").value)
-        addBookToLibrary(book);
-        addRow(book)
-        closeForm()
 
-    })
+//adding new book using the button
+
+button.addEventListener("click", () => {
+  popUp.style.display = "block"; //shows the popup
+  document.getElementById("btn").addEventListener("click", () => {
+    var book = new Book(
+      document.getElementById("author").value,
+      document.getElementById("title").value,
+      document.getElementById("pages").value,
+      document.getElementById("status").value
+    );
+    addBookToLibrary(book);
+    constructRows(mylib);
+    closeForm();
+  });
+  document.getElementById("close_button").addEventListener("click", () => {
+    closeForm();
+  });
 });
 
 // dynamically adding rows to existing table via js
 
-function addRow(book) {
-    var table = document.getElementById("table");
+function constructRows(mylib) {
+  console.log(mylib)
+  var table = document.getElementById("table");
+  for (let i = table.rows.length - 1; i > 0; i--) {
+    table.deleteRow(i);
+  }
+
+
+  mylib.forEach((book) => {
     var row = table.insertRow();
 
-    var cellData = [book.author,book.title,book.pages,book.status]
+    var cellData = [book.author, book.title, book.pages, book.status];
 
-    cellData.forEach((data)=>{
-        var cell = document.createElement("td")
-        cell.innerHTML= data;
-        row.appendChild(cell)  
-
-    })
-
+    cellData.forEach((data) => {
+      var cell = document.createElement("td");
+      cell.innerHTML = data;
+      row.appendChild(cell);
+    });
+  });
 
 }
 
@@ -57,7 +78,10 @@ function addRow(book) {
 //     popUp.style.display = "none"
 // })
 
-function closeForm()
-{
-    popUp.style.display = "none"
+function closeForm() {
+  popUp.style.display = "none";
+  document.getElementById("author").value = "";
+  document.getElementById("title").value = "";
+  document.getElementById("pages").value = "";
+  document.getElementById("status").value = "";
 }
